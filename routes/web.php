d54fc9725c2e2d1fd03cdc90cb88d96d4b265fc5
cursosdesarrolloweb <developer@cursosdesarrolloweb.es>
2026-02-28 14:11:34 +0100

    refactor: reorganize routes file

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,9 @@ Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
 
 Route::get('/services', function () {
     return view('services');
